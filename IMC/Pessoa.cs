1d6a30b6372e092109246350882c7ed5c5ce03c1
Ryan Reis <ryanreis280903@gmail.com>
2023-04-12 16:13:36 -0300

    Primeiro console em POO

---
+++ IMC/Pessoa.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMC
+{
+    internal class Pessoa
+    {
+        public double peso;
+        public double altura;
+
+        public double calculaIMC()
+        {
+            return peso / (altura * altura);
+        }
+        public string situacao(double valorIMC)
+        {
+            string retorno;
+
+            if (valorIMC < 18.5)
+            {
+                retorno = "Abaixo do peso";
+            }
+            else if (valorIMC < 25)
+            {
+                retorno = "Peso normal";
+            }
+            else if (valorIMC < 30)
+            {
+                retorno = "Acima do peso";
+            }
+            else if (valorIMC < 35)
+            {
+                retorno = "Obesidade 1";
+            }
+            else if (valorIMC < 40)
+            {
+                retorno = "Obesidade 2";
+            }
+            else
+            {
+                retorno = "Obesidade 3";
+            }
+            return retorno;
+        }
+        public void mensagem()
+        {
+            double obterCalculo = calculaIMC();
+            string obterSituacao = situacao(obterCalculo);
+            Console.WriteLine($"Seu peso é {obterCalculo} e está {obterSituacao}");
+            Console.ReadLine();
+        }
+
+    }   
+
+}
---
+++ IMC/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            p1.altura = 1.80;
+            p1.peso = 65;
+            p1.mensagem();
+        }
+    }
+}
